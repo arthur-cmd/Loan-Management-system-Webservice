ababfc64b518242d24bcee8f8994ac1f4eb2e117
arthur.cmd <arthurbenjamin27@gmail.com>
2023-02-12 15:00:30 +0300

    new repo

---
+++ src/test/java/io/credable/loanmanagementsystem/LoanManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package io.credable.loanmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
